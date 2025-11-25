a3b435359284727d1adad51d3f26023ee8ac744c
moneer-cpu <moneermubark4212@gmail.com>
2025-11-25 22:40:01 +0200

    Add customer_invoice_views.xml for invoice customization

--- dhs_res_partner_custom/__manifest__.py
+++ dhs_res_partner_custom/__manifest__.py
@@ -2,9 +2,10 @@
     'name': 'DHS Res Partner Custom',
     'version': '1.0',
     'summary': 'Add Nphies, Contract Number, CCHI fields for customers only',
-    'depends': ['base'],
+    'depends': ['sale'],
     'data': [
         'views/res_partner_views.xml',
+        'views/customer_invoice_views.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
