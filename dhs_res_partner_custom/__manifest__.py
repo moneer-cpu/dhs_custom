3db7e21a1216ccac2ca2b060d01fd1966fbd8571
moneer-cpu <moneermubark4212@gmail.com>
2025-12-02 11:11:22 +0200

    Customer invoice template

--- dhs_res_partner_custom/__manifest__.py
+++ dhs_res_partner_custom/__manifest__.py
@@ -1,10 +1,12 @@
 {
     'name': 'DHS Res Partner Custom',
     'version': '1.0',
-    'summary': 'Add Nphies, Contract Number, CCHI fields for customers only',
-    'depends': ['base'],
+    'summary': 'Add Nphies, Contract Number, CCHI fields for customers ',
+    'depends': ['l10n_sa_edi'],
     'data': [
         'views/res_partner_views.xml',
+        'views/customer_invoice_views.xml',
+        'report/report_invoice_inherit.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
