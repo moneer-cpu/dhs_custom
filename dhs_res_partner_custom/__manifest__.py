e369b5d32014fc9eea2d046f57c8728a1d828c8b
moneer-cpu <moneermubark4212@gmail.com>
2025-12-03 12:56:01 +0200

    Add provider_code field and enhance customer search view with CCHI and NPHIES fields

--- dhs_res_partner_custom/__manifest__.py
+++ dhs_res_partner_custom/__manifest__.py
@@ -2,11 +2,12 @@
     'name': 'DHS Res Partner Custom',
     'version': '1.0',
     'summary': 'Add Nphies, Contract Number, CCHI fields for customers ',
-    'depends': ['l10n_sa_edi'],
+    'depends': ['base','l10n_sa_edi'],
     'data': [
         'views/res_partner_views.xml',
         'views/customer_invoice_views.xml',
         'report/report_invoice_inherit.xml',
+        'views/partner_search_view.xml',
     ],
     'license': 'LGPL-3',
     'installable': True,
